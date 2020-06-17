441b37152447e1282abcc7133f07519c2ade8170
Mohammad540c <66434093+Mohammad540c@users.noreply.github.com>
2020-06-17 20:42:48 +0300

    done

---
+++ class work 2.playground/Contents.swift
@@ -0,0 +1,19 @@
+let family = ["ahmed", "mohammad"]
+
+var even = [2, 4, 6, 8, 10, 12 ]
+
+var grades = [90.3, 93.0, 88.7]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0])
+print(family[1])
+
+print(even[0])
+print(even[5])
+
+print(grades[0])
+print(grades[2])
+
+print(trueFalse[0])
+print(trueFalse[3])
---
+++ p2.playground/Contents.swift
@@ -0,0 +1,11 @@
+var fruits = ["mango", "orenge", "potato", "peach" ]
+
+print(fruits[0], fruits[3])
+fruits.append("strawberry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+fruits += ["banana", "watermelon", "pinapple", "kiwi"]
+print(fruits)
+fruits.removeAll()
+print(fruits)
